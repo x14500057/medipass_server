7065ef342d773092618c9be394d3dac6a9b4724f
Paul Byrne <paulbyrne@Pauls-iMac.local>
2019-07-23 00:44:04 +0100

    updated[A

--- controllers/medi_key.js
+++ controllers/medi_key.js
@@ -115,7 +115,7 @@ exports.alterMediRingStatus = async function(req, res ) {
         newStatus = 0;
     }
     else {
-        newStatus = 0
+        newStatus = 1
     }
     const deactSql = `UPDATE MediRing SET status = '0' WHERE PatientID = ? && status = ?`
 
